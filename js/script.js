f92c4c8e1f75f0e76c0c2e8bea48e6b7a5201c79
Tyler Sammons <tsammons@nd.edu>
2020-05-24 22:50:54 -0500

    refactor

--- js/script.js
+++ js/script.js
@@ -1,58 +1,31 @@
 "use strict";
 var ctx, canvas;
+canvas = document.getElementById('myCanvas');
+ctx = myCanvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.lineWidth = 1;
+ctx.strokeStyle = '#7CFC00';
 
 var depth = 16,
     branchSize = 6,
     branchAngle = 20,
-    deg_to_rad = Math.PI / 180.0;
-
-function init() {
-    canvas = document.getElementById('myCanvas');
-    ctx = myCanvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#7CFC00';
-    drawTree(depth);
-}
+    deg_to_rad = Math.PI / 180.0,
+    startPt = {x: canvas.width / 2, y: canvas.height},
+    startAngle = -95;
 
 function drawTree(depth) {
-    let treePts = buildTree(depth);
-    let treeProgress = [];
-    for (var seed = 0; seed < treePts.length; seed++) {
-        treeProgress.push([]);
-    }
-
+    let entireTree = buildTree(depth);
+    let treeProgress = populateProgressArray(entireTree);
     let currentBranch = 0;
+
     let myInterval = setInterval(() => {
-        if (currentBranch >= treePts.length) {
+        if (currentBranch >= entireTree.length) {
             clearInterval(myInterval);
         }
 
-        if (treeProgress[currentBranch].length == 0) {
-            for (var populate = 0; populate < treePts[currentBranch].length; populate++) {
-                let branchNode = treePts[currentBranch][populate];
-                let coords = {x: branchNode.start[0], y: branchNode.start[1], x2: branchNode.end[0], y2: branchNode.end[1]};
-                let newBranchTracker = {
-                    length: Math.sqrt((branchNode.start[0] - branchNode.end[0])*(branchNode.start[0] - branchNode.end[0]) + (branchNode.start[1] - branchNode.end[1])*(branchNode.start[1] - branchNode.end[1])),
-                    slope:   (branchNode.end[1] - branchNode.start[1]) / (branchNode.end[0] - branchNode.start[0]),
-                    coords: coords,
-                    growthSpeed: 2 + (2 * Math.random()),
-                    lengthDrawn: 0,
-                };
-                treeProgress[currentBranch].push(newBranchTracker);
-            }       
-        }
-
-        let progressCheck = [];
-        for (var i = 0; i < treeProgress[currentBranch].length; i++) {
-            if (treeProgress[currentBranch][i].lengthDrawn >= treeProgress[currentBranch][i].length) {
-                progressCheck.push(true);
-            } else {
-                progressCheck.push(false);
-            }
-        }
-        if (progressCheck.every((val, i, arr) => val === true )) {
+        let branchComplete = isBranchComplete(currentBranch, treeProgress);
+        if (branchComplete) {
             currentBranch += 1;
             return;
         }
@@ -60,48 +33,57 @@ function drawTree(depth) {
         for (var i = 0; i < treeProgress[currentBranch].length; i++) {
             let node = treeProgress[currentBranch][i];
             node.lengthDrawn += node.growthSpeed;
-            let endPt = pointFromSlopeLength(node.coords, node.slope, node.lengthDrawn);
-            drawLine(node.coords.x, node.coords.y, endPt.x, endPt.y);
+            let nextPoint = pointFromSlopeLength(node.coords, node.slope, node.lengthDrawn);
+            drawLine(node.coords.x1, node.coords.y1, nextPoint.x, nextPoint.y);
         }
     }, 1);
 }
 
-function buildTree(numberOfBreaks) {
+function buildTree(treeDepth) {
     let allBranches = [];
-    for (var seed = 0; seed < numberOfBreaks; seed++) {
+    for (var seed = 0; seed < treeDepth; seed++) {
         allBranches.push([]);
     };
 
-    for (var i = 0; i < numberOfBreaks; i++) {
-        if (i == 0) {
-            let startAngle = -95;
-            let startX = canvas.width / 2;
-            let startY = canvas.height;
-            let newBranch = 
-            {
-                angle: startAngle,
-                start: [startX, startY],
-                end: [startX + getSecondX(startAngle, numberOfBreaks-i), startY + getSecondY(startAngle, numberOfBreaks-i)],
-            };
-            allBranches[i].push(newBranch);
+    for (var currentBranch = 0; currentBranch < treeDepth; currentBranch++) {
+        if (currentBranch == 0) {
+            let firstBranch = 
+                {
+                    angle: startAngle,
+                    start: {
+                        x: startPt.x, 
+                        y: startPt.y
+                    },
+                    end: {
+                        x: startPt.x + getSecondX(startAngle, treeDepth-currentBranch),
+                        y: startPt.y + getSecondY(startAngle, treeDepth-currentBranch)
+                    }
+                };
+            allBranches[currentBranch].push(firstBranch);
         } else {
-            let lastEnds = allBranches[i-1];
-            for (var k = 0; k < lastEnds.length; k++) {
-                let parentBranch = lastEnds[k];
+            let parentBranches = allBranches[currentBranch-1];
+            for (var i = 0; i < parentBranches.length; i++) {
+                let parentBranch = parentBranches[i];
                 let childBranchA = {
                     angle: parentBranch.angle - branchAngle,
                     start: parentBranch.end,
-                    end: [parentBranch.end[0] + getSecondX(parentBranch.angle - (branchAngle * Math.random()), numberOfBreaks - i), parentBranch.end[1] + getSecondY(parentBranch.angle - (branchAngle * Math.random()), numberOfBreaks - i)],
+                    end: {
+                        x: parentBranch.end.x + getSecondX(parentBranch.angle - (branchAngle * Math.random()), treeDepth - currentBranch),
+                        y: parentBranch.end.y + getSecondY(parentBranch.angle - (branchAngle * Math.random()), treeDepth - currentBranch)
+                    }
                 };
                 let childBranchB = {
                     angle: parentBranch.angle + branchAngle,
                     start: parentBranch.end,
-                    end: [parentBranch.end[0] + getSecondX(parentBranch.angle + (branchAngle * Math.random()), numberOfBreaks - i), parentBranch.end[1] + getSecondY(parentBranch.angle + (branchAngle * Math.random()), numberOfBreaks - i)],
+                    end: {
+                        x: parentBranch.end.x + getSecondX(parentBranch.angle + (branchAngle * Math.random()), treeDepth - currentBranch),
+                        y: parentBranch.end.y + getSecondY(parentBranch.angle + (branchAngle * Math.random()), treeDepth - currentBranch)
+                    }
                 };
                 if (Math.random() > 0.5) {
-                    allBranches[i].push(childBranchA);
+                    allBranches[currentBranch].push(childBranchA);
                 }
-                allBranches[i].push(childBranchB);
+                allBranches[currentBranch].push(childBranchB);
             }
         }
     }
@@ -116,14 +98,14 @@ function getSecondY(angle, depth) {
     return Math.sin(angle * deg_to_rad) * depth * branchSize;
 }
 
-function pointFromSlopeLength(start, slope, length) {
+function pointFromSlopeLength(coords, slope, length) {
     let dx = (length / Math.sqrt(1 + (slope * slope)));
-    let x2 = start.x + (Math.sign(start.x2 - start.x) * dx);
-    let y2 = start.y + (Math.abs(slope) * dx * Math.sign(start.y2 - start.y));
+    let nextX = coords.x1 + (Math.sign(coords.x2 - coords.x1) * dx);
+    let nextY = coords.y1 + (Math.abs(slope) * dx * Math.sign(coords.y2 - coords.y1));
     if (!isFinite(slope)) {
-        y2 = start.y + length;
+        nextY = coords.y1 + length;
     }
-    return {x: x2, y: y2};
+    return {x: nextX, y: nextY};
 }
 
 function drawLine(x1, y1, x2, y2){
@@ -133,5 +115,38 @@ function drawLine(x1, y1, x2, y2){
     ctx.stroke();
 }
 
-init();
+function populateProgressArray(entireTree) {
+    let treeProgress = [];
+    for (var i = 0; i < entireTree.length; i++) {
+        treeProgress.push([]);
+        for (var j = 0; j < entireTree[i].length; j++) {
+            let branchNode = entireTree[i][j];
+            let branch = {
+                length: Math.sqrt((branchNode.start.x - branchNode.end.x)*(branchNode.start.x - branchNode.end.x) + (branchNode.start.y - branchNode.end.y)*(branchNode.start.y - branchNode.end.y)),
+                slope: (branchNode.end.y - branchNode.start.y) / (branchNode.end.x - branchNode.start.x),
+                coords: {
+                    x1: branchNode.start.x, 
+                    y1: branchNode.start.y, 
+                    x2: branchNode.end.x, 
+                    y2: branchNode.end.y
+                },
+                growthSpeed: 2 + (2 * Math.random()),
+                lengthDrawn: 0,
+            };
+            treeProgress[i].push(branch);
+        }
+    }
+    return treeProgress;
+}
+
+function isBranchComplete(currentBranch, treeProgress) {
+    for (var i = 0; i < treeProgress[currentBranch].length; i++) {
+        if (treeProgress[currentBranch][i].lengthDrawn < treeProgress[currentBranch][i].length) {
+            return false
+        }
+    }
+    return true;
+}
+
+drawTree(depth);
 
